7593c3b7f84bea03fa2f408adb5d158cf56462fd
ktollonr <agetor3210@naver.com>
2026-08-28 01:22:31 +0900

    init : create spring boot project

---
+++ src/test/java/com/giut/server/GiutServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.giut.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GiutServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
