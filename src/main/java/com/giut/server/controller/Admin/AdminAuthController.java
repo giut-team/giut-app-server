c6049604b7e258cb4f407ec71626c9a93f376431
ktollonr <agetor3210@naver.com>
2026-08-31 23:51:23 +0900

    feat: 학교 이메일 인증 메일 발송 기능 추가

--- src/main/java/com/giut/server/controller/Admin/AdminAuthController.java
+++ src/main/java/com/giut/server/controller/Admin/AdminAuthController.java
@@ -23,7 +23,7 @@ public class AdminAuthController {
 
     private final AuthService authService;
 
-    @PostMapping("/signup")
+    @PostMapping({"/signup", "/signup"})
     @Operation(summary = "관리자 전용 회원가입", description = "관리자 전용 계정을 생성합니다.")
     public ResponseEntity<SignUpResponse> signUp(@Valid @RequestBody SignUpRequest request) {
         return ResponseEntity.ok(authService.signUp(request));
