3926d3b5a05e4c037b36994175e3141adc81ca47
KimJungMook <wjdanr0869@naver.com>
2026-09-08 15:59:05 +0900

    refactor : N+1 문제 해결을 위한 EntityGraph 도입

--- src/main/java/com/giut/server/repository/UserProfileRoleRepository.java
+++ src/main/java/com/giut/server/repository/UserProfileRoleRepository.java
@@ -2,12 +2,14 @@ package com.giut.server.repository;
 
 import com.giut.server.entity.UserProfileRole;
 import com.giut.server.entity.UserProfileRoleId;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
 public interface UserProfileRoleRepository extends JpaRepository<UserProfileRole, UserProfileRoleId> {
 
+    @EntityGraph(attributePaths = "role")
     List<UserProfileRole> findAllByUserId(Long userId);
 
     void deleteByUserId(Long userId);
